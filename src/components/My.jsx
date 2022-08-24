70cb76c9355e42f676fc79d998d782d7baccb117
ankitmawle <ankitmawle@gmail.com>
2022-08-24 07:36:21 +0530

    Added Transfer and TransferAndUnpack Feture

--- src/components/My.jsx
+++ src/components/My.jsx
@@ -12,34 +12,116 @@ const My=()=>{
         </h2>
         </section>
 
-        <section id="nfts" className="min-h-[100vh]">
-        <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[300px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
-        <h3 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
-          Please provide us with details to get back your tokens
-        </h3>
-        <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white my-10">
-          <label class="relative block p-3 border-2 border-gray-200 rounded-lg" for="name">
-          <input
-            class="text-black  w-full px-0 pt-3.5 pb-0 text-sm placeholder-transparent border-none focus:ring-0 peer"
-            id="unpackID"
-            type="number"
-            placeholder="0"
-          />
+        <section id="nfts" className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+          <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[300px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+            <h3 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+              Please provide us with details to get back your tokens
+            </h3>
+            <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white my-10">
+              <label class="relative block p-3 border-2 border-gray-200 rounded-lg" for="name">
+                <input
+                  class="text-black  w-full px-0 pt-3.5 pb-0 text-sm placeholder-transparent border-none focus:ring-0 peer"
+                  id="unpackID"
+                  type="number"
+                  placeholder="0"
+                />
 
-          <span class="absolute text-xs font-medium text-gray-500 transition-all left-3 peer-focus:text-xs peer-focus:top-3 peer-focus:translate-y-0 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-sm">
-            Tokeniz ID
-          </span>
-        </label>
-        </p>
-        <button onClick={unpack} class="inline-block p-[2px] rounded bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:text-white active:text-opacity-75 focus:outline-none focus:ring" >
-        <span class="block px-8 py-3 text-sm font-medium bg-white rounded-sm hover:bg-transparent">
-          Unpack
-        </span>
-        </button>
-       
-      </div>
+                <span class="absolute text-xs font-medium text-gray-500 transition-all left-3 peer-focus:text-xs peer-focus:top-3 peer-focus:translate-y-0 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-sm">
+                  Tokeniz ID
+                </span>
+              </label>
+            </p>
+            <button onClick={unpack} class="inline-block p-[2px] rounded bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:text-white active:text-opacity-75 focus:outline-none focus:ring" >
+              <span class="block px-8 py-3 text-sm font-medium bg-white rounded-sm hover:bg-transparent">
+                Unpack
+              </span>
+            </button>
+          
+          </div>
 
-        </section>
+          <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[300px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+            <h3 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+              Please provide us with details to Send your NFT
+            </h3>
+            <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white my-10">
+              <label class="relative block p-3 border-2 border-gray-200 rounded-lg" for="name">
+              <input
+                class="text-black  w-full px-0 pt-3.5 pb-0 text-sm placeholder-transparent border-none focus:ring-0 peer"
+                id="transferID"
+                type="number"
+                placeholder="0"
+              />
+
+              <span class="absolute text-xs font-medium text-gray-500 transition-all left-3 peer-focus:text-xs peer-focus:top-3 peer-focus:translate-y-0 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-sm">
+                Tokeniz ID
+              </span>
+            </label>
+            </p>
+            <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white my-10">
+              <label class="relative block p-3 border-2 border-gray-200 rounded-lg" for="name">
+              <input
+                class="text-black  w-full px-0 pt-3.5 pb-0 text-sm placeholder-transparent border-none focus:ring-0 peer"
+                id="transferAddress"
+                type="text"
+                placeholder="0"
+              />
+
+              <span class="absolute text-xs font-medium text-gray-500 transition-all left-3 peer-focus:text-xs peer-focus:top-3 peer-focus:translate-y-0 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-sm">
+                Address of Receiver
+              </span>
+            </label>
+            </p>
+            <button onClick={transfer} class="inline-block p-[2px] rounded bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:text-white active:text-opacity-75 focus:outline-none focus:ring" >
+            <span class="block px-8 py-3 text-sm font-medium bg-white rounded-sm hover:bg-transparent">
+              Send
+            </span>
+            </button>
+          
+          </div>
+
+
+          <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[300px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+            <h3 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+              Please provide us with details to Send and Unpack the Tokeniz on Receiver's wallet
+            </h3>
+            <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white my-10">
+              <label class="relative block p-3 border-2 border-gray-200 rounded-lg" for="name">
+              <input
+                class="text-black  w-full px-0 pt-3.5 pb-0 text-sm placeholder-transparent border-none focus:ring-0 peer"
+                id="transferAndUID"
+                type="number"
+                placeholder="0x..."
+              />
+
+              <span class="absolute text-xs font-medium text-gray-500 transition-all left-3 peer-focus:text-xs peer-focus:top-3 peer-focus:translate-y-0 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-sm">
+                Tokeniz ID
+              </span>
+            </label>
+            </p>
+
+            <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white my-10">
+              <label class="relative block p-3 border-2 border-gray-200 rounded-lg" for="name">
+              <input
+                class="text-black  w-full px-0 pt-3.5 pb-0 text-sm placeholder-transparent border-none focus:ring-0 peer"
+                id="transferAndUAddress"
+                type="text"
+                placeholder="0x..."
+              />
+
+              <span class="absolute text-xs font-medium text-gray-500 transition-all left-3 peer-focus:text-xs peer-focus:top-3 peer-focus:translate-y-0 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-sm">
+              Address of Receiver
+              </span>
+            </label>
+            </p>
+            <button onClick={transferAndUnpack} class="inline-block p-[2px] rounded bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:text-white active:text-opacity-75 focus:outline-none focus:ring" >
+            <span class="block px-8 py-3 text-sm font-medium bg-white rounded-sm hover:bg-transparent">
+              Transfer And Unpack
+            </span>
+            </button>
+          
+        </div>
+
+      </section>
     </>
     )
 }
